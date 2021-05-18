57efea3c9793dcec181134c5e47e395547f7115c
allpavel <74268211+allpavel@users.noreply.github.com>
2021-05-18 17:38:03 +0300

    Add most part of functionallity

---
+++ src/Components/SearchResult/SearchResult.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SearchResult.css';
+import { TrackList } from "../TrackList/TrackList";
+
+export class SearchResult extends React.Component {
+    render() {
+        return (
+        <div className="SearchResults">
+            <h2>Results</h2>
+            <TrackList tracks={this.props.searchResult} onAdd={this.props.onAdd} isRemoval={false}/>
+        </div>
+        )
+    }
+}
